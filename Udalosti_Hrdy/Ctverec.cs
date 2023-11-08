60af7f0785d92a84f3cab754c11bdb33d1fe521e
KOVUCZ <Hrdy.Adam@stud.vassboskovice.cz>
2023-11-08 12:16:30 +0100

    timer

--- Udalosti_Hrdy/Ctverec.cs
+++ Udalosti_Hrdy/Ctverec.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Media;
+using System.Windows.Threading;
 
 namespace Udalosti_Hrdy
 {
@@ -22,7 +23,19 @@ namespace Udalosti_Hrdy
 
 
         private int vybranaBarva = 0;
+        private DispatcherTimer timer;
+        private Random random = new Random();
+        
         public double Velikost { get; private set; }
+        public Brush Barva { get => barvy[vybranaBarva]; }
+        
+        
+        public Ctverec(double velikost)
+        {
+            Velikost = velikost;
+            timer = new DispatcherTimer();
+            timer.Interval = new TimeSpan(0, 0, random.Next(1, 6));
+        }
 
     }
 }
