e6f187211259fa9ee21cc55a08a8924102c2bcd7
KOVUCZ <Hrdy.Adam@stud.vassboskovice.cz>
2023-11-08 12:09:28 +0100

    public class "ctverec"

---
+++ Udalosti_Hrdy/Ctverec.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace Udalosti_Hrdy
+{
+    public class Ctverec
+    {
+        private List<Brush> barvy = new List<Brush>()
+        {
+            Brushes.Red,
+            Brushes.Green,
+            Brushes.Blue,
+            Brushes.Magenta,
+            Brushes.Yellow,
+            Brushes.Orange,
+            Brushes.Orchid,
+        };
+
+
+        private int vybranaBarva = 0;
+        public double Velikost { get; private set; }
+
+    }
+}
