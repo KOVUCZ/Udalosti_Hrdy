39b5786d82c56d944ae7d98fe8fb8bc790b758cb
KOVUCZ <Hrdy.Adam@stud.vassboskovice.cz>
2023-11-22 13:21:35 +0100

    čtverce se generují a mění barvu podle vestavěného časovače

--- Udalosti_Hrdy/MainWindow.xaml.cs
+++ Udalosti_Hrdy/MainWindow.xaml.cs
@@ -28,15 +28,49 @@ namespace Udalosti_Hrdy
             InitializeComponent();
         }
 
-        public void GenerujCtvere(int pocet)
+        public void GenerujCtverce(int pocet)
         {
+            ctverce.Clear();
             for (int i = 0; i < pocet; i++)
             {
                 Ctverec ctverec = new Ctverec(random.Next(30,70));
-                var x = random.NextDouble();
-                ctverec.Umisteni = new Point();
+                var x = random.NextDouble() * (Platno.ActualWidth - ctverec.Velikost);
+                var y = random.NextDouble() * (Platno.ActualHeight - ctverec.Velikost);
+                ctverec.Umisteni = new Point(x, y);
+                ctverce.Add(ctverec);
+                ctverec.BarvaZmenena += Ctverec_BarvaZmenena;
 
             }
         }
+
+        private void Ctverec_BarvaZmenena()
+        {
+            NaklesliCtverce();
+        }
+
+        public void NaklesliCtverce()
+        {
+            Platno.Children.Clear();
+            foreach(Ctverec ctverec in ctverce)
+            {
+                Rectangle rectangle = new Rectangle();
+                rectangle.Width = ctverec.Velikost;
+                rectangle.Height = ctverec.Velikost;
+                rectangle.Fill = ctverec.Barva;
+                rectangle.Stroke = Brushes.Black;
+                rectangle.StrokeThickness = 3;
+                Canvas.SetLeft(rectangle, ctverec.Umisteni.X);
+                Canvas.SetTop(rectangle, ctverec.Umisteni.Y);
+                Platno.Children.Add(rectangle);
+            }
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            GenerujCtverce(20);
+            NaklesliCtverce();
+        }
+
+
     }
 }
